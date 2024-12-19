34662c364d64761c3e39c6bf2b5f0e999fa25675
akki-256 <168257172+akki-256@users.noreply.github.com>
2024-12-19 14:00:38 +0900

    Create table

---
+++ models/prace.py
@@ -0,0 +1,15 @@
+from peewee import Model, TextField, FloatField,DateField,IntegerField
+from .db import db
+
+
+class Restaurant(Model):
+    day = DateField(formats='%Y-%m-%d %H:%M:%S')
+    name = TextField(unique=True)
+    address = TextField()
+    lat = FloatField()
+    lon = FloatField()
+    evaluation = IntegerField()
+    comment = FloatField()
+
+    class Meta:
+        database = db
