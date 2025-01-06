df631e323e136bae703abffc34a646fbcb7841fd
satooru65536 <ryuto4619@gmail.com>
2025-01-06 12:29:28 +0900

    場所取得APIのエンドポイントを追加

--- routes/api.py
+++ routes/api.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models.prace import prace
+from playhouse.shortcuts import model_to_dict
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
+
+
+@api_bp.route("/points", methods=["GET"])
+def get_points():
+    points = [model_to_dict(p) for p in prace().select()]
+    return jsonify(points)
